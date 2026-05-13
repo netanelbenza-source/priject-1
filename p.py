b4afb2f50031143797985d2ccab223aeb406e3e2
netanel <netanelbenza@gmail.com>
2026-05-13 21:27:23 +0300

    add all game

--- p.py
+++ p.py
@@ -2,21 +2,24 @@ import random
 
 
 def Checking_the_input_of_level():
-     stap=None
+     Remaining_attempts = 0
+     stap = None
      while stap == None:
       your_choise_level = input("").lower()
+      if your_choise_level == "easy":
+          Remaining_attempts = 5
+      else:
+          Remaining_attempts = 3
       if your_choise_level == "easy" or your_choise_level== "hard" :
-          return your_choise_level
+            return your_choise_level , Remaining_attempts
+      print("enter only easy or hard ")
            
           
- 
-
-
 def Checking_the_input():
-     stap=None
+     stap = None
      while stap == None:
       your_choise_gass = input("").lower()
-      if your_choise_gass.isalpha() and "z" >= your_choise_gass >= "a" and len(your_choise_gass)==1:
+      if "z" >= your_choise_gass >= "a" and len(your_choise_gass)==1:
           return your_choise_gass
       elif len(your_choise_gass) > 1 :
           print("Enter input that contains only one character") 
@@ -25,36 +28,26 @@ def Checking_the_input():
 
       
      
-
-
-
-def Checking_caunt_the_word(word,leter):
+def Checking_count_the_word(word,leter):
     return word.count(leter)
-    
 
 
-def Checking_index_woile_count_1(word,leter):
+def Checking_index_while_count_1(word,leter):
     return word.index(leter)
    
     
 
-
-
-
-def Checking_index_woile_count_mor_1(word,leter,caunter1):
-    caunter_1 = -1
+def Checking_index_while_count_mor_1(word, leter, counter1):
+    counter_1 = -1
     indexes = []
-    for i in range(caunter1):
-        ind=word.index(leter ,caunter_1+1)
+    for i in range(counter1):
+        ind = word.index(leter ,counter_1+1)
         indexes.append(ind)
-        caunter_1=ind
+        counter_1=ind
     return indexes    
         
 
     
-
-
-
 def Return_list_of_word(choise):
     if choise == "easy":
         return  ["pizza",
@@ -71,58 +64,57 @@ def Return_list_of_word(choise):
     "knowledge",]
 
 
+def filling_the_notes(status_word, the_word_for_game , gass_of_plyer ):
+            count_the_word = Checking_count_the_word(the_word_for_game,gass_of_plyer)
+            if count_the_word == 1:
+                index_1=Checking_index_while_count_1(the_word_for_game,gass_of_plyer)
+                status_word[index_1] = gass_of_plyer
+                return status_word
+            else:
+                 index_more_1=Checking_index_while_count_mor_1(the_word_for_game,gass_of_plyer,count_the_word)
+                 for item in index_more_1:
+                     status_word[item] = gass_of_plyer
+                 return status_word    
+             
+
+def the_menu():
+    print("To START the game enter  1  ")
+    print("To EXIT from the game enter any other key")
+
+
+def get_status(status_word,Remaining_attempts,letters_he_guessed):
+        print(f"The status word is :  {"".join(status_word)}") 
+        print(f"The Remaining_attempts is : {Remaining_attempts} ")
+        print(f"The letters_he_guessed is : {"".join(letters_he_guessed)} ")
 
 
 
-
-
-
-
-
-
-def statr_game():
+def start_game():
     status_word = []
     Remaining_attempts = 0
     letters_he_guessed = []
     exit = 0
-    caunt=0
-    
+    count = 0
+
     while exit == 0:
-            
-        if caunt==0:
-
-            print("to START the game enter  1  ")
-            print("to EXIT from the game enter any other key")
-
+        if count == 0:
+            the_menu()
             your_choise = input("please enter yout choise ")
             if your_choise != "1":
                 exit += 1
                 continue
         
-    
-
             print("choise the level the game : easy or hard")
             your_choise_level = Checking_the_input_of_level()
-            if your_choise_level=="easy":
-                Remaining_attempts=5
-            else:
-                Remaining_attempts=3
-
-            list_the_curent_game = Return_list_of_word(your_choise_level)
-
+            Remaining_attempts = your_choise_level[1]
+            list_the_curent_game = Return_list_of_word(your_choise_level[0])
             the_word_for_game = list(random.choice(list_the_curent_game))
-            status_word =list(len(the_word_for_game)* "*")
-            caunt += 1
+            status_word = list(len(the_word_for_game)* "*")
+            count += 1
         
-        
-        print(the_word_for_game)
-        print(f"the status word is {"".join(status_word)}") 
-        print(f"the Remaining_attempts is {Remaining_attempts} ")
-        print(f"the letters_he_guessed is {"".join(letters_he_guessed)} ")
-
+        get_status(the_word_for_game,status_word,Remaining_attempts,letters_he_guessed)
         if Remaining_attempts == 0:
             print("You lost the game!!! The amount of guesses for the game has run out")
- 
             exit += 1
             continue
 
@@ -134,16 +126,10 @@ def statr_game():
         
 
         if gass_of_plyer in the_word_for_game:
+            filling=filling_the_notes(status_word, the_word_for_game , gass_of_plyer)
+            status_word = filling
            
-            caunt_the_word = Checking_caunt_the_word(the_word_for_game,gass_of_plyer)
-            if caunt_the_word == 1:
-                index_1=Checking_index_woile_count_1(the_word_for_game,gass_of_plyer)
-                status_word[index_1] = gass_of_plyer
-            else:
-                 index_more_1=Checking_index_woile_count_mor_1(the_word_for_game,gass_of_plyer,caunt_the_word)
-                 for item in index_more_1:
-                     status_word[item] = gass_of_plyer
-            if status_word ==  the_word_for_game :
+            if status_word == the_word_for_game :
                 print(f"Well done, you won the game :the word is {"".join(the_word_for_game)} ") 
                 exit+=1
                 continue
@@ -152,9 +138,10 @@ def statr_game():
                 letters_he_guessed.append(gass_of_plyer) 
                 Remaining_attempts -= 1
                 continue
+            print("You have already selected this letter")
                    
                 
-statr_game()
+start_game()
 
 
 
